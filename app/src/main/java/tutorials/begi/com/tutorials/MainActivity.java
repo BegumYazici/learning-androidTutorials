51928967e79a195fc73538d26d38c5fc935b28c8
Begum.Yazici <yazicibegum@gmail.com>
2018-09-11 23:24:29 +0300

    Commited

---
+++ app/src/main/java/tutorials/begi/com/tutorials/MainActivity.java
@@ -0,0 +1,13 @@
+package tutorials.begi.com.tutorials;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
